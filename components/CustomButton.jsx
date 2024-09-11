328527a48eca3597b22e27377463257dc9fbe53c
JaumDark <viniciuspkfr@gmail.com>
2024-09-10 21:27:23 -0300

    feat: add home page and tabs

---
+++ components/CustomButton.jsx
@@ -0,0 +1,12 @@
+import { Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const CustomButton = ({ text, handlePress, containerStyles, textStyles, isLoading }) => {
+  return (
+    <TouchableOpacity activeOpacity={0.7} onPress={handlePress} className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`} disabled={isLoading}>
+      <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>{text}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CustomButton
